55ff923975a921c8a26d80beee080727dcb1d252
aditokmo <adit.github@gmail.com>
2026-03-26 01:15:35 +0100

    fix: prevent crash when JWT is invalid

--- backend/src/utils/jwtVerify.ts
+++ backend/src/utils/jwtVerify.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 import { PrivateRequest } from "../types";
+import asyncHandler from "express-async-handler";
 
 interface TokenInterface {
     UserInfo: {
@@ -9,15 +10,21 @@ interface TokenInterface {
     }
 }
 
-export const protect = async (req: PrivateRequest, res: Response, next: NextFunction) => {
+export const protect = asyncHandler(async (req: PrivateRequest, res: Response, next: NextFunction) => {
     const headersJwt = req.headers?.authorization?.split(" ")?.at(1);
-    if (!headersJwt) return res.status(401).json({ status: 'error', message: 'Unauthorized' });
+    if (!headersJwt) {
+        res.status(401).json({ status: 'error', message: 'Unauthorized' });
+        return;
+    }
 
     const token = jwt.verify(headersJwt, process.env.ACCESS_TOKEN!) as TokenInterface;
 
     const user = await User.findById(token.UserInfo.id);
-    if (!user) return res.status(401).json({ status: 'error', message: 'Unauthorized' });
+    if (!user) {
+        res.status(401).json({ status: 'error', message: 'Unauthorized' });
+        return;
+    }
 
     req.id = user._id.toString()
     next();
-};
+});
